652e9fdcbcf62261c185f9e1fefdf3a06163903d
Bashar Eid <bashar@withclad.com>
2026-09-10 21:09:18 -0400

    configure markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import remarkBreaks from "remark-breaks";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkBreaks],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
